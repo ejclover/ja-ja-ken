24f867fabc77df30d461abefaa69e2bd7df1e307
ejclover <smithearl37@gmail.com>
2015-11-05 23:12:43 +0000

    inital commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,15 +1,30 @@
 var userPoint = 0;
 var aiPoint = 0;
-
+var aiChoice= 
 // This function returns the selection of the computer
 function getAISelection() {
+    
+    var weapon= Math.random()*3;
+    if(weapon<1){
+        
+        aiValue="rock";
+    }else if(weapon>1&&weapon<2){
+        aiValue="paper";
+        
+    }else{
+        aiValue="scissors";
+        
+    }
+    
     //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
     //TODO: pick the correct winner: user or ai
     //TODO: Add one point for the winner
+    
 }
 
 // This function sets the scoreboard with the correct points
@@ -19,6 +34,9 @@ function setScore() {
 
 // This function captures the click and picks the winner
 function evaluate(evt) {
+    
+    $("#scissors").on("click")
+    
     var userValue = evt.target.getAttribute('id');
     var aiValue = getAISelection();
 
